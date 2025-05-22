ca39df6d4e3757b26b8c0f3da57e8a685aceeded
Mohit Bhardwaj <mohitshukla0986@gmail.com>
2025-05-22 16:06:52 +0530

    fix

--- src/components/Trainding.jsx
+++ src/components/Trainding.jsx
@@ -11,7 +11,7 @@ function Trainding() {
       <div className="pt-5 mb-5">
        <SearchBtn/>
       </div>
-      <div className="w-full xl:overflow-y-scroll flex flex-wrap gap-5 px-4">
+      <div className="w-full xl:h-screen xl:overflow-y-scroll flex flex-wrap gap-5 px-4">
         {Array.isArray(data?.data?.items) && data.data.items.length > 0 ? (
           data.data.items.map((card) => <Card key={card.id} card={card} />)
         ) : (
